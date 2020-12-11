31336384a177fcf87016990ac0d768a4cd5afe60
asaulters <71015650+asaulters@users.noreply.github.com>
2020-12-11 05:44:12 -0500

    Add files via upload
    And the budget saga continues...

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 //Selectors and Vars
 let totalExpense;
-let expensesNew = [];
 let incomingCalcButton = document.getElementById('incomingCalcButton');
 let outgoingCalcButton = document.getElementById('expenseButton');
+let newExpenseName =  document.querySelector("[name=expense_Name]").value;
+let newExpenseAmount =  document.querySelector("[name=expense_Value]").value;
+let expensesNameSection = document.querySelector('#title_Expenses');
+let expensesAmountSection = document.querySelector('#value_Expenses');
+
 
 //Functions 
 
-function incomingCash(ev) {
+function incomingCash() {
     event.preventDefault();
     let incomingInput = document.querySelector("[name=incoming_Cash_Input]").value;
     let expenseInputName = document.querySelector("[name=expense_Name]").value;
@@ -17,34 +21,38 @@ function incomingCash(ev) {
 }
 
 
-const addExpense = (ev) => {
+function addExpense(event) {
+    //Prevent submission
     event.preventDefault();
-    let expense =[{
-        name: document.querySelector("[name=expense_Name]").value,
-        amount: document.querySelector("[name=expense_Value]").value
-    }]
-    expensesNew.push(expense[name]);
-    
-    for (let i = 0; i < expensesNew.length; i++){
-        console.log(expensesNew[0]);
-        console.log(expense.name);
-        console.log(expense.amount);
-        document.querySelector('#title_Expenses').innerHTML = expensesNew.name;
-        document.querySelector('#value_Expenses').innerHTML = expensesNew[amount];
-        let message = `You've just added ${expensesNew[name]} for .`;
-        console.log(message);
-    }
-    console.warn('added', {expensesNew});
-
-    //document.querySelector('#title_Expenses').innerHTML = expensesNew[i].name;
-    //document.querySelector('#value_Expenses').innerHTML = expensesNew[amount];
-    //let message = `You've just added ${expensesNew[name]} for .`;
-    // console.log(message);
+    //Expense Name Div
+    const expenseNameDiv = document.createElement('div');
+    expenseNameDiv.classList.add('expName');
+    //Expense amonut div
+    const expenseAmountDiv = document.createElement('div');
+    expenseAmountDiv.classList.add('expAmount');
+    //Create LI Expense Names
+    const newExpName = document.createElement('li');
+    newExpName.innerText = newExpenseName.value;
+    newExpName.classList.add('expense-name');
+    expenseNameDiv.appendChild(newExpName);
+    //Create LI Expense Amounts
+    const newExpAmt = document.createElement('li');
+    newExpAmt.innerText = newExpenseAmount.value;
+    newExpAmt.classList.add('expense-amount');
+    expenseNameDiv.appendChild(newExpAmt);
+    //append to LI
+    expensesNameSection.appendChild(expenseNameDiv);
+    expensesAmountSection.appendChild(expenseAmountDiv);
+    //Clear input
+    newExpenseName.value = "";
+    newExpenseAmount.value = "";
 }
 
 // Event Handlers
 incomingCalcButton.addEventListener("click", incomingCash);
 expenseButton.addEventListener("click", addExpense);
+
+
 
 
 // working js from before to keep in mind so I can clean up
@@ -69,3 +77,18 @@ expenseButton.addEventListener("click", addExpense);
 // }
 
 
+//const addExpense = (event) => {
+    // event.preventDefault();
+    // let expense =[{
+    //     name: document.querySelector("[name=expense_Name]").value,
+    //     amount: document.querySelector("[name=expense_Value]").value
+    // }]
+    // expensesNew.push(expense[name]);
+    
+    // console.warn('added', {expensesNew});
+
+    //document.querySelector('#title_Expenses').innerHTML = expensesNew[i].name;
+    //document.querySelector('#value_Expenses').innerHTML = expensesNew[amount];
+    //let message = `You've just added ${expensesNew[name]} for .`;
+    // console.log(message);
+//}
